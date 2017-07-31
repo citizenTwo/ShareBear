7cae705e9cf5488a0026a8b9bdeaeea9759472b8
Vaibhav Anand <vaibhav.anand123@gmail.com>
2017-07-31 21:00:40 +0530

    Mike Test. Hello 321 Hello

--- app/src/main/java/xyz/mrdeveloper/sharebear/LoginActivity.java
+++ app/src/main/java/xyz/mrdeveloper/sharebear/LoginActivity.java
@@ -141,6 +141,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 //AccessToken accessToken = loginResult.getAccessToken();
+                
                 Profile profile = Profile.getCurrentProfile();
                 nextActivity(profile);
                 Toast.makeText(getApplicationContext(), "Logging in...", Toast.LENGTH_SHORT).show();
